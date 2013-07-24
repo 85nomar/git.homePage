9865d721039ba43c26784274c404b0c1bbfed24f
Nobel Martinez <nobel.martinez@ricardo.ch>
2013-07-24 02:53:17 +0200

    set project structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require('inc/layout.php');
+
+$Layout->title = 'Sellingform';
+
+$Layout->start(); ?>
+
+
+<section id="container" class="container">
+
+
+</section>
+
+<?php $Layout->end(); ?>
